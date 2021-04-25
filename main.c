20f005025c1628df8a73d9aac3667cc759f2d3fa
teoretik-eugene <74543647+teoretik-eugene@users.noreply.github.com>
2021-04-25 18:51:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     }
 
     int ch;     // Переменная для считывания файла
-
+    printf("Введенный аргумент: \n");
     if(!argv[2]){
         printf("Error in bracket-argument\n\n");
     }
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
         printf(argv[2]);
         printf("\n");
     }
+    printf("Предупреждения: \n");
         int br=0;   // Перемещение по строке
         while (argv[2][br]!='\0'){
 
@@ -32,6 +33,11 @@ int main(int argc, char *argv[])
                 rewind(input);
                 int fig_open = 0, fig_close = 0, count_brackets = 0;
                 while((ch=getc(input))!=EOF){
+                    if(ch=='"'){            // Проверка на кавычки
+                        do{
+                            ch=getc(input);
+                        }while(ch!='"');
+                    }
                     if(ch=='[' || ch==']' || ch=='(' || ch==')')
                         count_brackets++;
 
@@ -44,6 +50,8 @@ int main(int argc, char *argv[])
                         count_brackets++;
                     }
                 }
+                //printf("%d", fig_open); printf("\n");
+                //printf("%d", fig_close); printf("\n");
                 if(fig_open > fig_close){
                     printf("Лишние {\n");
                     break;
@@ -54,8 +62,8 @@ int main(int argc, char *argv[])
                         break;
                     }
                 }
-                char *string = (char*) malloc(count_brackets+1);
-                rewind(input);
+                char *string = (char*) malloc(count_brackets+1);    // Уж переделай тогда..
+                rewind(input);                                    // ..string на fig
                 int j = 0;
                 while((ch=getc(input))!=EOF){
                     if(ch=='{'){
@@ -93,24 +101,51 @@ int main(int argc, char *argv[])
                         }
                     }
                 }
+                free(string);
                 rewind(input);
                 break;
+
 
             /*Проверка на круглые скобки*/
             case '(':
                 // Изначально тут было int m = 0, но выдавало ошибку
                 rewind(input);
-                int circ_count = 0;
+                int circ_count = 0, circ_open = 0, circ_close = 0;
                 while((ch=getc(input))!=EOF){
-                    if(ch=='(' || ch == ')')
+                    if(ch=='"'){
+                        do{
+                            ch=getc(input);
+                        }while(ch!='"');
+                    }
+
+                    if (ch == '{' || ch == '}' || ch == '[' || ch == ']')
                         circ_count++;
+
+                    if(ch == '('){
+                        circ_open++;
+                        circ_count++;
+                    }
+                    if(ch == ')'){
+                        circ_close++;
+                        circ_count++;
+                    }
                 }
-                int m = 0;
+                if(circ_open > circ_close){
+                    printf("Лишние (\n");
+                    break;
+                }
+                if(circ_close > circ_open){
+                    printf("Лишние )\n");
+                    break;
+                }
+
+                int m = 0;          // Переменная для символьного массива
                 char *circ = (char*) malloc(circ_count+1);
                 rewind(input);
                 while((ch=getc(input))!=EOF){
+
                     if(ch=='('){
-                        circ[m]='(';
+                        circ[m]='(';        // Ошибка, ( в конце
                         m++;
                     }
                     if(ch=='{'){
@@ -144,6 +179,7 @@ int main(int argc, char *argv[])
                         }
                     }
                 }
+                free(circ);
                 rewind(input);
                 break;
 
@@ -156,7 +192,10 @@ int main(int argc, char *argv[])
         br++;
     }
 
+    rewind(input);      // Тут не было rewind и выдавало ошибку
+
     /*Отображение содержимого файла*/
+    printf("\n-------------------------------\n");
     printf("Исходный файл: \n");
     while((ch = getc(input))!=EOF){
         printf("%c", ch);
@@ -165,7 +204,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-
-
-
