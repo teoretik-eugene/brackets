ec72a06924d6b05484d6c7e7aab949e897b93f7d
teoretik-eugene <74543647+teoretik-eugene@users.noreply.github.com>
2021-05-02 01:18:30 +0300

    Add files via upload
    Новая версия ДЗ по скобкам

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
     setlocale( LC_ALL,"Russian" );
-    /*РћС‚РєСЂС‹С‚РёРµ Рё РїСЂРѕРІРµСЂРєР° Р°СЂРіСѓРјРµРЅС‚РѕРІ: */
+
+    /* Проверка открытого файла: */
     FILE *text;
     if((text = fopen(argv[1], "r"))==NULL){
         printf("ERROR IN OPENING FILE\n");
@@ -15,208 +16,203 @@ int main(int argc, char *argv[])
         printf("ERROR IN ARG 2\n");
         exit(1);
     }
-    printf("Р’РІРµРґРµРЅРЅР°СЏ СЃС‚СЂРѕРєР°-Р°СЂРіСѓРјРµРЅС‚:\n");
-    printf("%s", argv[2]);printf("\n");
+    printf("Введенная строка-аргумент:\n");
+    printf("%s", argv[2]);printf("\n---------------------\n");
 
-    /*РЎРѕР·РґР°РґРёРј РјР°СЃСЃРёРІ СЃРёРјРІРѕР»РѕРІ, СЃРѕСЃС‚РѕСЏС‰РёР№ РёР· СЃРєРѕР±РѕРє СЂР°Р·РЅС‹С… С‚РёРїРѕРІ*/
+    int ch; // Указатель для перемещения по файлу
 
-    /*РџРѕСЃС‡РёС‚Р°РµРј РѕР±С‰РµРµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРєРѕР±РѕРє*/
-    int count_brackets = 0, ch;
+    /* Выведем файл в консоль */
+    while((ch = getc(text))!= EOF){
+        printf("%c", ch);
+    }
+    printf("\n---------------------\n");
+    rewind(text);
+
+    /* Вычисляем количество скобок, за исключением содержания в кавычках */
+    int count_brackets = 0;     // Колисество скобок всех типов
+    int fig_bracket = 0, circ_bracket = 0, square_bracket = 0;
     while((ch=getc(text))!=EOF){
-        if(ch=='"'){
+        if (ch == '"'){
             do{
-                ch=getc(text);
-            }while(ch!='"');
+                ch = getc(text);
+            }while(ch != '"');
         }
-        if(ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='[' || ch==']')
+        if(ch == '{' || ch == '}'){
             count_brackets++;
+            fig_bracket++;
+        }
+        if(ch == '(' || ch == ')'){
+            count_brackets++;
+            circ_bracket++;
+        }
+        if(ch == '[' || ch == ']'){
+            count_brackets++;
+            square_bracket++;
+        }
     }
     rewind(text);
 
-    char *string_br = (char *) malloc(count_brackets+1);//Р’С‹РґРµР»СЏРµРј РїРѕРґ РјР°СЃСЃРёРІ РїР°РјСЏС‚СЊ
-
-    /*Р—Р°РїРёС€РµРј РІ РјР°СЃСЃРёРІ РІСЃРµ СЃРєРѕР±РєРё, Р·Р° РёСЃРєР»СЋС‡РµРЅРёРµРј СЃРѕРґРµСЂР¶Р°РЅРёСЏ РІ РєР°РІС‹С‡РєР°С…*/
-    int i = 0;
+    /* Создадим и выведем массив символов скобок */
+    char *string_brackets = (char *) malloc (count_brackets + 1);
+    int j = 0;
     while((ch=getc(text))!=EOF){
-        if(ch=='"'){
+        if (ch == '"'){
             do{
-                ch=getc(text);
-            }while(ch!='"');
+                ch = getc(text);
+            }while(ch != '"');
         }
-        if(ch=='{' || ch=='}' || ch=='(' || ch==')' || ch=='[' || ch==']'){
-            string_br[i] = ch;
-            i++;
+        if(ch == '{' || ch == '}' || ch == '(' || ch == ')' || ch == '[' || ch == ']'){
+            string_brackets[j] = ch;
+            j++;
         }
     }
+    string_brackets[j] = '\0';
+    printf("Массив символов: \n");
+    printf("%s", string_brackets);
     rewind(text);
-    string_br[i]='\0';
-    printf("\n");
-    printf("%s", string_br);printf("\n");   // Р’С‹РІРѕРґ РјР°СЃСЃРёРІР° СЃРёРјРІРѕР»РѕРІ РЅР° СЌРєСЂР°РЅ
 
-    printf("РџСЂРµРґСѓРїСЂРµР¶РґРµРЅРёСЏ: \n");
-    /*РџСЂРѕС…РѕРґРёРјСЃСЏ РїРѕ Р°СЂРіСѓРјРµРЅС‚Сѓ СЃРєРѕР±РѕРє*/
+    printf("\nПредупреждения: \n");
+    /*Проходимся по аргументу скобок*/
     int br = 0;
     while(argv[2][br]!='\0'){
-        /*РќР°С‡РёРЅР°РµС‚СЃСЏ Р°РЅР°Р»РёР· СЃРєРѕР±РѕРє РІ Р°СЂРіСѓРјРµРЅС‚Рµ*/
         switch(argv[2][br]){
 
-            /*РџСЂРѕРІРµСЂРєР° С„РёРіСѓСЂРЅС‹С… СЃРєРѕР±РѕРє*/
-            case '{':
-                rewind(text);
-                int fig_open = 0, fig_close = 0;
-                /*РџСЂРѕРІРµСЂРєР° РЅР° РєРѕР»РёС‡РµСЃС‚РІРѕ*/
-                int j=0;
-                while(string_br[j]!='\0'){
-                    if(string_br[j]=='{')
-                        fig_open++;
-
-                    if(string_br[j]=='}')
-                        fig_close++;
-                    j++;
+                    /* Проверка фигурных скобок */
+        case '{':
+            j = 0;
+            int f = 0, k = 0;
+            char *fig_string = (char *) malloc(fig_bracket+1);
+            while(string_brackets[j]!='\0'){
+                if(string_brackets[j] == '{'){
+                    fig_string[f] = '{';
+                    f++;
+                    k++;
                 }
-                if(fig_open > fig_close){
-                    printf("Р›РёС€РЅРёРµ {\n");
+
+                if(string_brackets[j] == '}' && fig_string[f-1] != '{'){
+                    fig_string[k] = '}';
+                    --f;
+                    k++;
+                }
+
+                if(string_brackets[j]=='}' && fig_string[f-1] == '{'){
+                    fig_string[f-1] = ' ';
+                    --f;
+                }
+                j++;
+            }
+            //fig_string[k+1]='\0';
+            //printf("\n");printf("%s", fig_string);printf("\n");
+            f = 0; int err_f = 0; // err1 служит для фиксации ошибки
+            for(int i = 0; fig_string[i]!='\0'; i++){
+                if(fig_string[i] == '{'){
+                    printf("Не хватает закрывающей }\n");
+                    err_f++;
                     break;
                 }
-                if(fig_open < fig_close){
-                    printf("Р›РёС€РЅРёРµ }\n");
+                if(fig_string[i] == '}'){
+                    printf("Не хватает открывающей {\n");
+                    err_f++;
                     break;
                 }
-                /*
-                Р•СЃР»Рё РЅРµС‚ Р»РёС€РЅРёС… РѕС‚РєСЂС‹С‚С‹С… РёР»Рё Р»РёС€РЅРёС… Р·Р°РєСЂС‹С‚С‹С… СЃРєРѕР±РѕРє
-                С‚Рѕ РЅР°С‡РёРЅР°РµС‚СЃСЏ РїСЂРѕРІРµСЂРєР° РЅР° РїРµСЂРµСЃРµС‡РµРЅРёРµ
-                */
-                j=0;
-                while(string_br[j]!='\0'){
-                    if(string_br[j]=='}' && string_br[j-1]=='('){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: (}\n");
-                        break;
-                    }
-                    if(string_br[j]=='}' && string_br[j-1]=='['){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: [}\n");
-                        break;
-                    }
-                    if(string_br[j]=='{' && string_br[j+1]==')'){   //Р”РѕР±Р°РІР»РµРЅ СЃР»СѓС‡Р°Рё РїРµСЂРµСЃРµС‡РµРЅРёСЏ
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: {)\n");       // С‚РёРїР° {] Рё {)
-                        break;
-                    }
-                    if(string_br[j]=='{' && string_br[j+1]==']'){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: {]\n");
-                        break;
-                    }
-                    j++;
+            }
+            if(err_f>=1)
+                break;  // останавливает, если есть проблема с открывающимися или закрываюшимися
+                        // скобками
+            /*Проверка на пересечение с другими скобками*/
+            j=0;
+            while(string_brackets[j]!='\0'){
+                if(string_brackets[j]=='}' && string_brackets[j-1]=='('){
+                    printf("Пересечение типа: (}\n");
+                    break;
                 }
-            break;
-
-            /*
-                РђРЅР°Р»РѕРіРёС‡РЅРѕ СЂР°Р±РѕС‚Р°СЋС‚ Рё РѕСЃС‚Р°Р»СЊРЅС‹Рµ РїСЂРѕРІРµСЂРєРё РґР»СЏ РґСЂСѓРіРёС… СЃРєРѕР±РѕРє
-            */
-
-            /*РџСЂРѕРІРµСЂРєР° РЅР° РєСЂСѓРіР»С‹Рµ СЃРєРѕР±РєРё*/
-            case '(':
-                rewind(text);
-                int circ_open = 0, circ_close = 0;
-                //
-                /*РџСЂРѕРІРµСЂРєР° РЅР° РєРѕР»РёС‡РµСЃС‚РІРѕ*/
-                int c=0;
-                while(string_br[c]!='\0'){
-                    if(string_br[c]=='(')
-                        circ_open++;
-
-                    if(string_br[c]==')')
-                        circ_close++;
+                if(string_brackets[j]=='}' && string_brackets[j-1]=='['){
+                    printf("Пересечение типа: [}\n");
+                    break;
+                }
+                if(string_brackets[j]=='{' && string_brackets[j+1]==')'){   //Добавлен случаи пересечения
+                    printf("Пересечение типа: {)\n");       // типа {] и {)
+                    break;
+                }
+                if(string_brackets[j]=='{' && string_brackets[j+1]==']'){
+                    printf("Пересечение типа: {]\n");
+                    break;
+                }
+                j++;
+            }
+        break;
+                    /* Проверка круглых скобок */
+        case '(':
+            j=0;
+            int c = 0, m = 0;
+            char *circ_string = (char *) malloc(circ_bracket+1);
+            while(string_brackets[j]!='\0'){
+                if(string_brackets[j] == '('){
+                    circ_string[c] = '(';
                     c++;
+                    m++;
                 }
-                if(circ_open > circ_close){
-                    printf("Р›РёС€РЅРёРµ (\n");
-                    break;
-                }
-                if(circ_open < circ_close){
-                    printf("Р›РёС€РЅРёРµ )\n");
-                    break;
-                }
-                /*
-                Р•СЃР»Рё РЅРµС‚ Р»РёС€РЅРёС… РѕС‚РєСЂС‹С‚С‹С… РёР»Рё Р»РёС€РЅРёС… Р·Р°РєСЂС‹С‚С‹С… СЃРєРѕР±РѕРє
-                С‚Рѕ РЅР°С‡РёРЅР°РµС‚СЃСЏ РїСЂРѕРІРµСЂРєР° РЅР° РїРµСЂРµСЃРµС‡РµРЅРёРµ
-                */
-                c=0;
-                while(string_br[c]!='\0'){
-                    if(string_br[c]==')' && string_br[c-1]=='{'){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: {)\n");
-                        break;
-                    }
-                    if(string_br[c]==')' && string_br[c-1]=='['){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: [)\n");
-                        break;
-                    }
-                    if(string_br[c]=='(' && string_br[c+1]==']'){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: (]\n");
-                        break;
-                    }
-                    if(string_br[c]=='(' && string_br[c+1]=='}'){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: (}\n");
-                        break;
-                    }
-                    c++;
-                }
-            break;
 
-            /*РџСЂРѕРІРµСЂРєР° РЅР° РєРІР°РґСЂР°С‚РЅС‹Рµ СЃРєРѕР±РєРё*/
-            case '[':
-                rewind(text);
-                int square_open = 0, square_close = 0;
-                /*РџСЂРѕРІРµСЂРєР° РЅР° РєРѕР»РёС‡РµСЃС‚РІРѕ*/
-                int s=0;
-                while(string_br[s]!='\0'){
-                    if(string_br[s]=='[')
-                        square_open++;
+                if(string_brackets[j] == ')' && circ_string[c-1] != '('){
+                    circ_string[m] = ')';
+                    --c;
+                    m++;
+                }
 
-                    if(string_br[s]==']')
-                        square_close++;
-                    s++;
+                if(string_brackets[j] == ')' && circ_string[c-1] == '('){
+                    circ_string[c-1] = ' ';
+                    --c;
                 }
-                if(square_open > square_close){
-                    printf("Р›РёС€РЅРёРµ [\n");
+                j++;
+            }
+
+            c = 0; int err_c = 0; // err служит для фиксации ошибки
+            for(int i = 0; circ_string[i]!='\0'; i++){
+                if(circ_string[i] == '('){
+                    printf("Не хватает закрывающей )\n");
+                    err_c++;
+                    break;// думал break остановит case
+                }
+                if(circ_string[i] == ')'){
+                    printf("Не хватает открывающей (\n");
+                    err_c++;
                     break;
                 }
-                if(square_open < square_close){
-                    printf("Р›РёС€РЅРёРµ ]\n");
-                    break;
-                }
-                /*
-                Р•СЃР»Рё РЅРµС‚ Р»РёС€РЅРёС… РѕС‚РєСЂС‹С‚С‹С… РёР»Рё Р»РёС€РЅРёС… Р·Р°РєСЂС‹С‚С‹С… СЃРєРѕР±РѕРє
-                С‚Рѕ РЅР°С‡РёРЅР°РµС‚СЃСЏ РїСЂРѕРІРµСЂРєР° РЅР° РїРµСЂРµСЃРµС‡РµРЅРёРµ
-                */
-                s=0;
-                while(string_br[s]!='\0'){
-                    if(string_br[s]==']' && string_br[s-1]=='{'){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: {]\n");
+            }
+            if(err_c>=1)
+                break;  // останавливает, если есть проблема с открывающимися или закрываюшимися
+                        // скобками
+
+            /* Проверка на пересечение с другими скобками */
+            j=0;
+            while(string_brackets[j]!='\0'){
+                    if(string_brackets[j]==')' && string_brackets[j-1]=='{'){
+                        printf("Пересечение типа: {)\n");
                         break;
                     }
-                    if(string_br[s]==']' && string_br[s-1]=='('){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: (]\n");
+                    if(string_brackets[j]==')' && string_brackets[j-1]=='['){
+                        printf("Пересечение типа: [)\n");
                         break;
                     }
-                    if(string_br[s]=='[' && string_br[s+1]=='}'){   //Р”РѕР±Р°РІР»РµРЅС‹ СЃР»СѓС‡Р°Рё
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: [}\n");       // РїРµСЂРµСЃРµС‡РµРЅРёСЏ С‚РёРїР° [} Рё [)
+                    if(string_brackets[j]=='(' && string_brackets[j+1]=='}'){
+                        printf("Пересечение типа: (}\n");
                         break;
                     }
-                    if(string_br[s]=='[' && string_br[s+1]==')'){
-                        printf("РџРµСЂРµСЃРµС‡РµРЅРёРµ С‚РёРїР°: [)\n");
+                    if(string_brackets[j]=='(' && string_brackets[j+1]==']'){
+                        printf("Пересечение типа: (]\n");
                         break;
                     }
-                    s++;
-                }
-            break;
+                    j++;
+            }
+        break;
+
+                /* Проверка квадратных скобок */
+        case '[':
+            // остается тут дописать
+        break;
+
         }
-        br++;   // РћСЃСѓС‰РµСЃС‚РІР»СЏРµС‚ РїРµСЂРµРјРµС‰РµРЅРёРµ РїРѕ argv[2]
-    }
-
-    printf("\n");
-    rewind(text);
-    printf("\nРСЃС…РѕРґРЅС‹Р№ С„Р°Р№Р»: \n");
-    while((ch=getc(text))!=EOF){
-        printf("%c", ch);
+        br++;
     }
     return 0;
 }
